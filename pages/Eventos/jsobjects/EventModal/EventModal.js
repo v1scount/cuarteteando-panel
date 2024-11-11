5dfdfde5861a2ddd8e2236596d899910779b2d14
Rodrigo Vellido <rodrigovellido1@gmail.com>
2024-11-11 00:26:07 +0000

    System generated commit, initial commit

---
+++ pages/Eventos/jsobjects/EventModal/EventModal.js
@@ -0,0 +1,20 @@
+export default {
+	event: {
+    "name": "",
+    "description": "",
+    "artist": "",
+    "date": "",
+    "place": "",
+    "image": "",
+    "extraInformation": {}
+	},
+	setEvent (event) {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		this.event = event
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
---
+++ pages/Eventos/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	addEventVisibility: false,
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	},
+	switchEventModal () {
+		this.addEventVisibility = !this.addEventVisibility
+	}
+	
+}
